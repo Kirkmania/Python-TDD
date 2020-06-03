27ab9b52185d43b8ba4308190052076127cbe5cb
Kirkmania <george.kirkman27@gmail.com>
2020-06-03 20:04:09 +0100

    Extended FT, home.html with basic POST handling

--- functional_tests.py
+++ functional_tests.py
@@ -38,20 +38,26 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Play more Call of Duty' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        # NOTE: Cool stuff used to be here. f-string and any() function
+        self.assertIn('1: Play more Call of Duty', [row.text for row in rows])
 
         # There is still a text box inviting him to to add another item.
         # He enters "Make the blog look prettier"
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Make the blog look prettier')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # The page updates again, and now shows both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Play more Call of Duty', [row.text for row in rows])
+        self.assertIn('2: Make the blog look prettier', [row.text for row in rows])
 
         # George wonders whether the site will remember his list. Then he sees
         # that the site has generated a unique URL for him, with some explanatory
         # text to that effect.
+        self.fail('Finish the test!')
 
         # He visits that URL, his to-do list is still there.
 
--- lists/views.py
+++ lists/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+# Info on render request extra args in
+# https://www.obeythetestinggoat.com/book/chapter_post_and_database.html
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', ''),
+    })
