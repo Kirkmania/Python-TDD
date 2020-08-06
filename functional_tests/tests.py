a6dcddf7d3aa8b95a01d2925dc2583127a6179c9
Kirkmania <george.kirkman27@gmail.com>
2020-08-06 19:54:47 +0100

    added better explicit wait (chp6)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,11 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
 # import unittest replaced with django.test
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -14,10 +17,18 @@ class NewVisitorTest(LiveServerTestCase):
 
     # helper function for testing row text!
     # https://www.obeythetestinggoat.com/book/chapter_post_and_database.html
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
     
     def test_can_start_a_list_and_retrieve_it_later(self):
         # George has started along the Way of the Testing Goat and
@@ -42,8 +53,7 @@ class NewVisitorTest(LiveServerTestCase):
         # When he hits enter, the page updates, and now the page lists
         # "1: Play more Call of Duty" as an item in a to-do list NOTE: Cool stuff used to be here. f-string and any() function
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Play more Call of Duty')
+        self.wait_for_row_in_list_table('1: Play more Call of Duty')
 
         # There is still a text box inviting him to to add another item.
         # He enters "Make the blog look prettier"
@@ -53,8 +63,8 @@ class NewVisitorTest(LiveServerTestCase):
         time.sleep(1)
 
         # The page updates again, and now shows both items on her list
-        self.check_for_row_in_list_table('1: Play more Call of Duty')
-        self.check_for_row_in_list_table('2: Make the blog look prettier')
+        self.wait_for_row_in_list_table('1: Play more Call of Duty')
+        self.wait_for_row_in_list_table('2: Make the blog look prettier')
 
         # George wonders whether the site will remember his list. Then he sees
         # that the site has generated a unique URL for him, with some explanatory
